2bb6d0dccb7983890dfa8b09d6764a47d2f225ef
lihangyu <lihangyu6@huawei.com>
2025-01-08 17:47:17 +0800

    fix: modify security header

--- pkg/httpserver/middleware.go
+++ pkg/httpserver/middleware.go
@@ -47,13 +47,14 @@ func AccessLoggingMiddleware(next http.Handler) http.Handler {
 		start := time.Now()
 
 		// add http security header
-		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
-		w.Header().Set("X-Content-Type-Options", "nosniff")
-		w.Header().Set("X-XSS-Protection", "1; mode=block")
-		w.Header().Set("Referrer-Policy", "same-origin")
-		w.Header().Set("Content-Security-Policy", "connect-src 'self' https:;")
+		csp := "connect-src 'self' https:;frame-ancestors 'none';object-src 'none'"
+		w.Header().Set("Content-Security-Policy", csp)
 		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-XSS-Protection", "1")
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
+		w.Header().Set("Referrer-Policy", "same-origin")
 
 		// Create a new responseLogger
 		rl := &responseLogger{
